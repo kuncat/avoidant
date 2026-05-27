b01ec3c51683f995e0e56d4f995fa9483fe33612
Alvin Ali Khaled <aakside@gmail.com>
2026-05-27 00:34:40 -0400

    Use Open Sans font everywhere

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,9 @@
   let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
 {@render children()}
 
 <div style="display:none">
